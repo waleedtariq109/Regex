7a63c1849106f62799116c08811e89ce7acc4244
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-06-10 04:01:50 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Regular Expression Pattern Syntax.
+ *
+ * The Pattern of Regular expression is very simple
+ *
+ * The pattern begins with forward slash and pattern that is word/character
+ * along with some along with some search pattern like '[]', '*' etc and after
+ * that add forward slash and modifier or flag
+ *
+ * Example:
+ * /case/i
+ * Here you can see the pattern start with forward slash and the pattern after
+ * that add forward slash and add flag or modifier
+ *
+ * If the same pattern is to be used using 'RegExp()' method it should
+ * look like this.
+ *
+ * Example:
+ * RegExp("Pattern", "Flag or Modifier")
+ * or
+ * RegExp("case", "i")
+ *
+ * The RegExp method takes two arguments
+ * 1: Pattern
+ * 2: Flag or Modifier
+ */
